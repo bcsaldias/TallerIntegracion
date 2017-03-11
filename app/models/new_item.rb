c34932907766950ee924a075a3983c64323618ff
bcsaldias <bcsaldias@uc.cl>
2017-03-10 22:44:46 -0500

    Added counter comments

--- app/models/new_item.rb
+++ app/models/new_item.rb
@@ -2,4 +2,8 @@ class NewItem < ApplicationRecord
   has_many :comments
   validates :lead, length: {maximum: 250}, allow_blank: true
 
+  def count_comments
+    Comment.where(:new_item_id => self.id).count
+  end
+
 end

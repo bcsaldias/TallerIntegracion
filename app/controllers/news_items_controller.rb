b8929b5c814b0c5b291c1be2309ed150d8894276
bcsaldias <bcsaldias@uc.cl>
2017-03-13 00:55:05 -0400

    Updated removed news message

--- app/controllers/news_items_controller.rb
+++ app/controllers/news_items_controller.rb
@@ -63,9 +63,11 @@ class NewsItemsController < ApplicationController
   # DELETE /news_items/1
   # DELETE /news_items/1.json
   def destroy
+    @title = @news_item.title
     @news_item.destroy
     respond_to do |format|
-      format.html { redirect_to news_items_url, notice: 'Noticia eliminada exitosamente.' }
+      format.html { redirect_to news_items_url, notice: 'Noticia "'+ @title +\
+                            '" ha sido eliminada exitosamente junto a sus comentarios.' }
       format.json { head :no_content }
     end
   end
